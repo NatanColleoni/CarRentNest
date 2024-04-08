f0d984d3dfd7cf50bb30291c79fee5bd131d61ef
Natan Colleoni Fonseca <natancolleonifonseca@gmail.com>
2024-04-07 21:27:50 -0300

    Atividade com resultados utilizando docker

--- src/clusters.ts
+++ src/clusters.ts
@@ -2,19 +2,20 @@ const os = require('os');
 const cluster = require('cluster');
 
 const runPrimaryProcess = () => {
-  const processCount = (os.cpus().length * 2) + 3;
+  const processCount = 13;
   console.log(`Primary ${process.pid} is running`);
   console.log(`Forking Server with ${processCount} processes \n`);
   for (let i = 0; i < processCount; i++) {
     cluster.fork();
   }
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`)
-        console.log('Starting a new worker')
-        cluster.fork()
-    })
-
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`,
+    );
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
 };
 
 const runWorkerProcess = async () => {
